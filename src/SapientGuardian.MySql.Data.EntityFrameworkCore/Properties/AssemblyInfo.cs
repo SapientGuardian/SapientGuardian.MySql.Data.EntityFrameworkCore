1779d2557ec3ec002633fb370068a98fac6d27ba
Elijah Bate <elijahbate@gmail.com>
2016-07-13 13:00:24 +1000

    Change package namespace to be keep standard MySql.Data.* naming

--- src/SapientGuardian.MySql.Data.EntityFrameworkCore/Properties/AssemblyInfo.cs
+++ src/SapientGuardian.MySql.Data.EntityFrameworkCore/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MySql.Data.EntityFramework7")]
+[assembly: AssemblyProduct("MySql.Data.EntityFrameworkCore")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
